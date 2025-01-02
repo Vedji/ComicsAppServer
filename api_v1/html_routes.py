34575ef87525015b4e529a83514bc351611b91e3
Ponkratov Nickolay <45602069+Vedji@users.noreply.github.com>
2025-01-02 07:20:26 +0300

    Delete api_v1 directory

--- api_v1/html_routes.py
+++
@@ -1,8 +0,0 @@
-from flask import render_template
-from api_v1 import bp
-
-
-@bp.route('/append_book')
-def html_routes_append_book():
-    print("hello")
-    return render_template("test_add_book.html")
