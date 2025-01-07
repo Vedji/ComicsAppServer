a9728654a500629fedeb111790f764bfe9752b8c
Ponkratov Nickolay <45602069+Vedji@users.noreply.github.com>
2025-01-07 18:14:28 +0300

    Add files via upload

--- server/api/api_genre.py
+++ server/api/api_genre.py
@@ -6,6 +6,7 @@ from flask_jwt_extended import create_access_token, jwt_required, get_jwt_identi
 from sqlalchemy import or_, not_
 
 from server import Config
+from server.api_response import ApiResponse
 from server.api.extensions import ExtensionsReturned
 from server.models.db_books import DBBooks
 from server.models.db_files import DBFiles
@@ -20,20 +21,9 @@ genre_api = Blueprint('genre_api', __name__)
 @genre_api.route('/v1/genre/list', methods=['GET'])
 def get_genre_list():
     genres = DBGenre.query.all()
+    result = ApiResponse([ item.to_json() for item in genres ])
 
-    result = []
-    for item in genres:
-        result.append(
-            {
-                "genreID": item.genre_id,
-                "genreName": item.name
-            }
-        )
-
-    return {
-        "genreList": result,
-        "count": len(result)
-    }
+    return result.response()
 
 
 
